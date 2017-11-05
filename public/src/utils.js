590fd56068d5b1fbdd1062d4a22d51a5114d524e
liu <245613360@qq.com>
2017-11-05 21:47:23 +0800

    退出登录完成

--- public/src/utils.js
+++ public/src/utils.js
@@ -2,6 +2,7 @@
 define([
     'jquery'
 ], function($) {
+    //检测登录
     $.ajax({
         url: '/api/employee/checkRootLogin',
         type: 'get',
@@ -10,6 +11,18 @@ define([
                 location.href = '/login.html';
             }
         }
+    });
+
+    $('.logout').on('click', function () {
+        $.ajax({
+            url: '/api/employee/employeeLogout',
+            type: 'get',
+            success: function (info) {
+                if(info.success) {
+                    location.href = '/login.html'
+                }
+            }
+        })
     })
     
 });
